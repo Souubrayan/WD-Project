c4b3cac1e34d16b7654072f68e94c22be9e5b449
Souubrayan <34951540+Souubrayan@users.noreply.github.com>
2019-08-07 00:10:26 -0400

    Add files via upload

---
+++ PROYECTO FINAL DISENO WEB/Validacion.js
@@ -0,0 +1,13 @@
+function validar(){
+
+    var admin = "admin";
+    var user = "user";
+
+    if(document.Forms.admin == admin){
+        document.Form.txtusuario.focus()
+    }
+
+    if(document.Forms.user == user){
+        document.Form.txtusuario.focus() 
+    }
+}
